c290cebb6eeea88f9ab9d9cc03083517bca91841
Vidhan Bhatt <me@vidhan.io>
2022-09-06 13:16:04 -0400

    feat: add some more boilerplate stuff

---
+++ src/config.rs
@@ -0,0 +1,52 @@
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("{0}")]
+    Io(#[from] std::io::Error),
+    #[error("{0}")]
+    Toml(#[from] toml::de::Error),
+}
+
+/// The default configuration path.
+///
+/// This is <home dir>/.config/unii/config.toml.
+pub static DEFAULT_CONFIG_PATH: Lazy<PathBuf> = Lazy::new(|| {
+    dirs::home_dir()
+        .expect("Could not find home directory")
+        .join(".config/unii/config.toml")
+});
+
+/// Type for configuration options.
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Config {
+    /// The path to the root of the university work directory.
+    pub directory: PathBuf,
+}
+
+impl Config {
+    /// Create a new configuration with the given directory.
+    ///
+    /// # Errors
+    ///
+    /// This will error if the file cannot be read, or if the file is not valid TOML.
+    pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
+        let s = fs::read_to_string(path)?;
+        toml::from_str(&s).map_err(Into::into)
+    }
+
+    /// Create a new configuration with the given directory.
+    #[must_use]
+    pub fn from_dir<D: AsRef<Path>>(directory: D) -> Self {
+        Self {
+            directory: directory.as_ref().into(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
-use clap::Parser;
-use unii::course::Code;
+use std::{
+    fs::{self, create_dir_all},
+    path::PathBuf,
+};
+
+use clap::{ArgGroup, Parser};
+use unii::{
+    config::{self, Config},
+    course::{Code, Course},
+};
 
 #[derive(clap::Parser)]
+#[clap(group(
+            ArgGroup::new("config")
+                .args(&["directory", "config-file"]),
+        ))]
 struct Arguments {
     #[clap(subcommand)]
     subcommand: Subcommand,
+
+    #[clap(short, long, value_parser)]
+    directory: Option<PathBuf>,
+
+    #[clap(short, long, value_parser)]
+    config_file: Option<PathBuf>,
 }
 
 #[derive(clap::Subcommand)]
@@ -23,34 +41,68 @@ enum CourseSubcommand {
         description: Option<String>,
     },
     Delete {
-        code: String,
+        code: Code,
     },
     List,
 }
 
+impl CourseSubcommand {
+    const fn code(&self) -> Option<&Code> {
+        match self {
+            Self::Create { code, .. } | Self::Delete { code } => Some(code),
+            Self::List => None,
+        }
+    }
+}
+
 fn main() {
     let args = Arguments::parse();
+
+    let config = match (args.directory, args.config_file) {
+        (Some(directory), _) => Config::from_dir(directory),
+        (_, Some(config_file)) => Config::from_file(config_file).unwrap(),
+        (None, None) => {
+            let config_file = config::DEFAULT_CONFIG_PATH;
+            create_dir_all(
+                config_file
+                    .parent()
+                    .expect("default config should have parent"),
+            )
+            .expect("creating directories should not fail");
+
+            if !config_file.exists() {
+                fs::write(&*config_file, "").expect("creating config should not fail");
+            }
+
+            Config::from_file(config_file).expect("reading config should not fail")
+        }
+    };
+
     match args.subcommand {
-        Subcommand::Course(subcommand) => match subcommand {
-            CourseSubcommand::Create {
-                code,
-                name,
-                description,
-            } => {
-                println!("Creating course with code {}", code);
-                if let Some(name) = name {
-                    println!("Course name: {}", name);
+        Subcommand::Course(subcommand) => {
+            match subcommand {
+                CourseSubcommand::Create {
+                    code,
+                    name,
+                    description,
+                } => {
+                    let mut course = Course::new(code);
+
+                    if let Some(name) = name {
+                        course.set_name(name);
+                    }
+
+                    if let Some(description) = description {
+                        course.set_description(description);
+                    }
                 }
-                if let Some(description) = description {
-                    println!("Course description: {}", description);
+                CourseSubcommand::Delete { code } => {
+                    println!("Deleting course with code {}", code);
                 }
-            }
-            CourseSubcommand::Delete { code } => {
-                println!("Deleting course with code {}", code);
-            }
-            CourseSubcommand::List => {
-                println!("Listing courses");
-            }
-        },
+                CourseSubcommand::List => {
+                    println!("Listing courses");
+                }
+            };
+        }
     }
 }
